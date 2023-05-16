b9442637ee36bc940e976e9520d1fe84165b1b53
Mary Ann Jose <maryannjose129@gmail.com>
2023-05-16 11:03:00 +0530

    commit

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:mindful/homepage.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -10,52 +11,69 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  final p1 = TextEditingController();
+  final p2 = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Center(
-        child: Column(
-          children: [
-            SizedBox(height: 250),
-            Text("Sign Up",style: TextStyle(fontSize: 25),),
-            SizedBox(height: 40,),
-            Padding(
-              padding: const EdgeInsets.only(left:16.0,right:16.0),
-              child: TextField(
-                decoration: InputDecoration(
+        body: Center(
+      child: Column(
+        children: [
+          SizedBox(height: 250),
+          Text(
+            "Sign Up",
+            style: TextStyle(fontSize: 25),
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+            child: TextField(
+              decoration: InputDecoration(
                   labelText: "email addres",
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30)
-                  )
-                ),
-              ),
+                      borderRadius: BorderRadius.circular(30))),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left:16.0,right:16.0,top:10.0),
-              child: TextField(
-                decoration: InputDecoration(
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 10.0),
+            child: TextField(
+              controller: p1,
+              decoration: InputDecoration(
                   labelText: "password",
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30)
-                  )
-                ),
-              ),
+                      borderRadius: BorderRadius.circular(30))),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left:16.0,right:16.0,top:10.0),
-              child: TextField(
-                decoration: InputDecoration(
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 10.0),
+            child: TextField(
+              controller: p2,
+              decoration: InputDecoration(
                   labelText: "confirm password",
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(30)
-                  )
-                ),
-              ),
+                      borderRadius: BorderRadius.circular(30))),
             ),
-            
-          ],
-        ),
-      )
-    );
+          ),
+          SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () {
+              if (p1 == p2) {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => HomePage()));
+              } else {
+                AlertDialog();
+              }
+            },
+            child: Text("SIGN IN"),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.teal[300],
+              shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }

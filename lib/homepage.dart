d589a46bbe974fc6a4c1258dcd60a2748b9fd80e
Mary Ann Jose <maryannjose129@gmail.com>
2023-05-18 00:03:11 +0530

    commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -11,6 +11,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<String> il = <String>['1', '2', '3', '4', '5'];
+  bool moodTap=false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,14 +36,14 @@ class _HomePageState extends State<HomePage> {
           Padding(
             padding: const EdgeInsets.only(top: 20),
             child: Positioned(
-              child: Container(
-                child:Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                  IconButton(onPressed: (){}, icon: Icon(Icons.menu)),
-                  IconButton(onPressed:(){},icon:Icon(Icons.notifications))
-                ],)
-            )),
+                child: Container(
+                    child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.notifications))
+              ],
+            ))),
           ),
           Positioned(
               top: 140,
@@ -49,17 +51,63 @@ class _HomePageState extends State<HomePage> {
                 height: 800,
                 width: MediaQuery.of(context).size.width,
                 decoration: BoxDecoration(
+                    color: Colors.white,
                     boxShadow: [
                       BoxShadow(
-                          color: Color.fromARGB(255, 137, 122, 122),
+                          color: Colors.grey.shade600,
                           spreadRadius: 1,
-                          blurRadius: 15),
+                          blurRadius: 15)
                     ],
-                    color: Colors.white,
                     borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(30),
                         topRight: Radius.circular(30))),
-              ))
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 30, top: 30),
+                      child: Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            "Your Mood Today",
+                            style: TextStyle(
+                                fontSize: 23, fontWeight: FontWeight.w600),
+                          )),
+                    ),
+                    SizedBox(height: 25),
+                    Container(
+                      height:60,
+                        child: ListView.builder(
+                            scrollDirection: Axis.horizontal,
+                            itemCount: il.length,
+                            itemBuilder: (BuildContext context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.only(left:16),
+                                child: InkWell(
+                                  onTap: (){
+                                    moodTap=true;
+                                  },
+                                  child:Container(
+                                    height:60,
+                                    width:60,
+                                    child:Text('${il[index]}'),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      boxShadow: [
+                                         BoxShadow(
+                                           color: Color.fromARGB(255, 164, 159, 159),
+                                            spreadRadius: 1,
+                                           blurRadius: 15)
+                                      ],
+                                      border: moodTap?Border.all(color:Colors.black87):Border.all(color: Colors.transparent),
+                                      borderRadius: BorderRadius.circular(10)
+                                    ),
+                                  ),
+                                ),
+                              );
+                            })),
+                  ],
+                ),
+              )),
         ],
       ),
     ));
